6266dad46fad78f23083e51b7d39c8601a98e1d7
faraz1442 <111956082+faraz1442@users.noreply.github.com>
2022-08-24 12:32:04 +0530

    Update streamlit_app.py
    Added pandas in action

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas as pd
 
 streamlit.title("My parents New Healthy Diner")
 
@@ -8,3 +9,6 @@ streamlit.title("🥗Kale, Spinach & Rocket Smootie")
 streamlit.title("🐔Hard boiled free ranged egg")
 streamlit.title("🥑🍞Avocado Toast")
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
